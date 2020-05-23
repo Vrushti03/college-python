f60e46f7933b8b72a8d33df26648552f6a9edd95
Vrushti03 <65769635+Vrushti03@users.noreply.github.com>
2020-05-23 09:26:01 +0530

    Add files via upload

---
+++ assignment1/5.py
@@ -0,0 +1,4 @@
+a="the quick brown fox jumps over the lazy dog"
+b=a.split()
+c=len(b)
+print(c-1)
---
+++ assignment1/9.py
@@ -0,0 +1,6 @@
+S = input('Enter string:')
+
+if (S == S[::-1]):
+    print("palindrome")
+else:
+    print("not palindrome")
